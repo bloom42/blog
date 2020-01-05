a4f7a0209d903b4c4340fefa3ee72e890072994f
z0mbie42 <sylvain@kerkour.fr>
2020-01-05 14:25:25 +0100

    description

--- static/js/index.js
+++ static/js/index.js
@@ -2,19 +2,18 @@
 (function ($) {
 
   var previousScroll = 20;
+  var minimumScroll = 71;
       // scroll functions
       $(window).scroll(function(e) {
 
           // add/remove class to navbar when scrolling to hide/show
           var scroll = $(window).scrollTop();
-          if (scroll >= previousScroll) {
+          if (scroll >= previousScroll && scroll >= minimumScroll) {
               $('.navbar').addClass("navbar-hide");
-
-          }else if (scroll < previousScroll) {
+          } else if (scroll < previousScroll) {
               $('.navbar').removeClass("navbar-hide");
           }
           previousScroll = scroll;
-
       });
 
   })(jQuery);
