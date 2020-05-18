5328cf1abaf0f5e95f86a0f2663e99833ff4b1d0
z0mbie42 <sylvain@kerkour.fr>
2020-05-18 22:33:15 +0200

    new theme

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,4 +1,142 @@
+////////////////////////////////////////////////////////////////////////////////////////////////////
+// vars
+////////////////////////////////////////////////////////////////////////////////////////////////////
+window.apiBaseUrl = 'https://shibui.bloom42.com/api';
+// window.apiBaseUrl = 'http://localhost:8000/api';
+window.shibuiProjectId = 'ONUWIORPF5ZWQ2LCOVUS6UDSN5VGKY3UF4YQ';
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+// Functions
+////////////////////////////////////////////////////////////////////////////////////////////////////
+function _redirect(domain){
+  var hostname = location.hostname;
+  var path = location.pathname.replace('/blog', '');;
+  if (hostname != domain && hostname != 'localhost' && hostname != '127.0.0.1') {
+    location.href = 'https://' + domain + path;
+  }
+}
+
+function rot13(s) {
+  return (s ? s : this).split('').map(function(_){
+    if (!_.match(/[A-Za-z]/)) return _;
+    c = Math.floor(_.charCodeAt(0) / 97);
+    k = (_.toLowerCase().charCodeAt(0) - 83) % 26 || 26;
+    return String.fromCharCode(k + ((c == 0) ? 64 : 96));
+  }).join('');
+}
+
+
+function displayError(message) {
+  $('#fe-alert').html('<div class="alert alert-danger" role="alert">'
+    + message +
+  '</div>');
+  $('#fe-alert').show();
+}
+
+
+function displaySuccess(message) {
+  $('#fe-alert').html('<div class="alert alert-success" role="alert">'
+    + message +
+  '</div>');
+  $('#fe-alert').show();
+}
+
+function hideAlert() {
+  $('#fe-alert').hide();
+}
+
+function hideLoader() {
+  $('.loader').hide();
+}
+
+function showLoader() {
+  $('.loader').show();
+}
+
+function graphqlReq(query, variables) {
+  var payload = {
+    query: query,
+    variables: variables,
+  };
+  var data = {
+    data : JSON.stringify(payload),
+    contentType : 'application/json',
+    type : 'POST',
+  };
+  return $.ajax(window.apiBaseUrl+'/graphql', data);
+}
+
+function subscribeToNewsletter(email) {
+  showLoader();
+  hideAlert();
+
+  var query = 'mutation($input: SubscribeContactInput!) { subscribeContact(input: $input) }';
+  var variables = {
+    input: {
+      projectId: window.shibuiProjectId,
+      email: email,
+    },
+  };
+  graphqlReq(query, variables)
+  .done(function(data) {
+    if (data.errors && data.errors.length > 0) {
+      displayError(data.errors[0].message);
+      return ;
+    }
+    window.location.href = '/confirm';
+  })
+  .fail(function() {
+    displayError('Error subscribing. Please try again.');
+  })
+  .always(function() {
+    hideLoader();
+  })
+}
+
+function unsubscribe() {
+  hideAlert();
+  var urlParams = new URLSearchParams(window.location.search);
+  var token = urlParams.get('token');
+  var contactId = urlParams.get('contact');
+  if (token == null || contactId == null) {
+    hideLoader();
+    displayError('Error unsuscribing. Please click again on the link provided in the email.');
+    return;
+  }
+
+  var query = 'mutation($input: UnsubscribeContactInput!) { unsubscribeContact(input: $input) }';
+  var variables = {
+    input: {
+      id: decodeURIComponent(contactId),
+      token: token,
+    },
+  };
+  graphqlReq(query, variables)
+  .done(function(data) {
+    if (data.errors && data.errors.length > 0) {
+       displayError(data.errors[0].message);
+       return ;
+    }
+    displaySuccess('You no longer will receive our emails. Have a good day!');
+  })
+  .fail(function() {
+    displayError('Error unsuscribing. Please click again on the link provided in the email.');
+  })
+  .always(function() {
+    hideLoader();
+  })
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+// now
+////////////////////////////////////////////////////////////////////////////////////////////////////
+_redirect('fatalentropy.com');
+hideLoader();
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
 // document ready
+////////////////////////////////////////////////////////////////////////////////////////////////////
 $(document).ready(function() {
 
   // deofuscate emails
@@ -13,4 +151,56 @@ $(document).ready(function() {
   $(document.links).filter(function() {
     return this.hostname != window.location.hostname;
   }).attr('target', '_blank').attr('rel', 'noopener');
+
+
+  // subscribe
+  if ($("#fe-subscribe-button-input").length) {
+    $("#fe-subscribe-button-input").click(function(e) {
+      e.preventDefault();
+      subscribeToNewsletter($('#fe-subscribe-email-input').val());
+    });
+  }
+
+
+  if ($("#fe-thank-you").length) {
+    var urlParams = new URLSearchParams(window.location.search);
+    var token = urlParams.get('token');
+    var contactId = urlParams.get('contact');
+    if (token == null || contactId == null) {
+      hideLoader();
+      displayError('Error confirming your subscription. Please click again on the link provided in the email.');
+      return;
+    }
+
+    var query = 'mutation($input: VerifyContactInput!) { verifyContact(input: $input) }';
+    var variables = {
+      input: {
+        id: decodeURIComponent(contactId),
+        token: token,
+      },
+    };
+    graphqlReq(query, variables)
+    .done(function(data) {
+      if (data.errors && data.errors.length > 0) {
+         displayError(data.errors[0].message);
+         return ;
+      }
+      displaySuccess('You are now subscribed to <b>Open Source Weekly</b>');
+    })
+    .fail(function() {
+      displayError('Error confirming your subscription. Please click again on the link provided in the email.');
+    })
+    .always(function() {
+      hideLoader();
+    })
+  }
+
+
+  if ($("#osw-unsubscribe-btn").length) {
+    $("#osw-unsubscribe-btn").click(function(e) {
+      e.preventDefault();
+      unsubscribe();
+    });
+  }
+
 });
--- content/subscribe.md
+++ content/subscribe.md
@@ -5,24 +5,44 @@ date = 2019-02-05T01:42:42+02:00
 +++
 
 
-# Subscribe to {{< sitename >}}
+# Subscribe
+
+I’ll write you once a week about open source, sustainability and avoiding complexity. <br />
+
+<small>
+I hate spam even more than you do.
+I'll never share your email and you can unsubscribe at anytime. Also there is no tracking or ads.
+</small>
+
+<br />
+
+<noscript>
+  <strong>We're sorry but Fatal Entropy doesn't work properly without JavaScript enabled. Please enable it to continue.</strong>
+</noscript>
+
+<div id="fe-alert"></div>
+<div class="loader center" style="display: none;"></div>
+<form>
+  <div class="subscribe-form center">
+    <input type="email" id="fe-subscribe-email-input" aria-describedby="email-subscribe" placeholder="your@email.com">
+    <button id="fe-subscribe-button-input">Subscribe</button>
+  </div>
+</form>
+
 
 <br />
 
 ## RSS
 
-Love RSS? Use this URL in your favorite RSS reader: <br />
+Prefer RSS? Use this URL in your favorite RSS reader: <br />
 [{{< rssurl >}}]({{< rssurl >}})
-
-<br />
-
-## Twitter
-
-Follow
-<a href="https://twitter.com/@42bloom" target="_blank" rel="noopener">@42bloom</a> on Twitter to never miss one of our posts.
-
-<br />
 
 ## Mastodon
 
-<a href="https://mastodon.social/@42bloom" target="_blank" rel="me noopener">@42bloom@mastodon.social</a>
+ Prefer Mastodon? Follow <a href="https://mastodon.social/@42bloom" target="_blank" rel="me noopener">@42bloom@mastodon.social</a>
+
+
+## Twitter
+
+Prefer Twitter? Follow
+<a href="https://twitter.com/@42bloom" target="_blank" rel="noopener">@42bloom</a>
